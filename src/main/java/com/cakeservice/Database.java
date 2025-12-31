135925cae45d7f00259b0e1294f1e6e548ff8c23
Sara Backstrom <2360234@cegep-heritage.qc.ca>
2025-12-31 13:22:41 -0500

    Create Database class, update gitignore with properties file and create an example properties file for public reference

---
+++ src/main/java/com/cakeservice/Database.java
@@ -0,0 +1,25 @@
+package com.cakeservice;
+
+import java.io.InputStream;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class Database {
+
+    public static Connection getConnection() throws Exception {
+        Properties properties = new Properties();
+
+        try (InputStream in = Database.class.getClassLoader().getResourceAsStream("db,properties")) {
+            properties.load(in);
+        }
+
+        return DriverManager.getConnection(
+                properties.getProperty("db.url"),
+                properties.getProperty("db.user"),
+                properties.getProperty("db.password")
+        );
+    }
+}
+
+//This Database class will allow me to easily make connections to my database
